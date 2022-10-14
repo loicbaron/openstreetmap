af8de56005acc3e4cdd7a2b06ef3b3d52c703fa9
loicbaron <loic.baron@gmail.com>
2022-10-14 23:05:37 +0100

    Error after mapply

--- osm.R
+++ osm.R
@@ -12,16 +12,16 @@ get_osm <- function(loc_code, loc_name, loc_geo, key, value, features_type) {
   f <- str_glue("data/objects/{loc_code}")
   if (file.exists(f) == TRUE) {
     cat("already have features for: ", loc_name, "\n")
-    result <- readRDS(f)
+    osm_features <- readRDS(f)
   } else {
     cat("get osm features for: ", loc_name, "\n")
-    result <- opq(bbox = st_bbox(loc_geo), timeout = 1000) %>%
+    osm_features <- opq(bbox = st_bbox(loc_geo), timeout = 1000) %>%
       add_osm_feature(key, value) %>%
       osmdata_sf() %>%
       unname_osmdata_sf()
-    saveRDS(result, f)
+    saveRDS(osm_features, f)
   }
-  return(result)
+  return(osm_features)
 }
 
 emergency_services <- c("hospital", "police", "fire_station")
@@ -37,10 +37,17 @@ nb_er <- nrow(as.data.frame(er))
 C_GOV2 <- nb_er / locations$pop[1] * 100000
 st_write(er, "output/er_polygons.shp", layer_options = "ENCODING=UTF-8", append=FALSE)
 
-emergencies <- mapply(get_osm,
+
+# Error in `[[<-.data.frame`(`*tmp*`, i, value = list("20.979242,105.716335,21.10758,105.804311",  :
+# replacement has 8 rows, data has 391
+# In addition: Warning message:
+#   In mapply(get_osm, locations$GEOLEVEL2, locations$ADM2_VI, locations$geometry,  :
+#               longer argument not a multiple of length of shorter
+locations$emergencies <- mapply(get_osm,
                       locations$GEOLEVEL2,
                       locations$ADM2_VI,
                       locations$geometry,
                       "amenity",
                       emergency_services
                       )
+
